bb8cde309d41233264bc7564cc4cf4f6b155e427
VITORIA GRAFF <vitoria.graff@edu.pucrs.br>
2024-03-22 18:56:03 -0300

    declaração de identificadores gerais

--- src/Colecionaveis.java
+++ src/Colecionaveis.java
@@ -1,4 +1,11 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Colecionaveis {
+    private String identificacooUnica;
+    private String nome;
+    private Date dataDeAquisicao;
+    private ArrayList<String> listaDeAutores;
 }
